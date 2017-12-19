7afd33fe823206f7806fdd1a4966fb9429fa6fb6
amykslover <amykslover@gmail.com>
2017-12-19 13:41:44 -0800

    Create react app push to github

---
+++ config/database.js
@@ -0,0 +1,7 @@
+
+// module.exports = {
+
+//     'url' : 'mysql://wogjgybl685giuc6:frql98kxyuqn223t@ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rv6j9azwfnlphgio' 
+//     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+// };
